9a294d5448d88387158d315248c280b53a15e1fa
Joenio Costa <joenio@colivre.coop.br>
2013-09-06 09:14:33 -0300

    show some global metrics

--- lib/Visualizo/Repository.pm
+++ lib/Visualizo/Repository.pm
@@ -18,10 +18,20 @@ sub visualization {
   my $url_sha1 = $self->param('url_sha1');
   my $repositories = CodeJuicer::DB->c('repositories')->find({ _id => $url_sha1 });
   my $repository = $repositories->next;
+  my $metrics = CodeJuicer::DB->c('metrics')->find({ _id => $url_sha1 });
+  my $metric = $metrics->next;
+  my $graphs = CodeJuicer::DB->c('graphs')->find({ _id => $url_sha1 });
+  my $total_files = @{ $graphs->next->{dsm_cluster1}->{nodes} };
   $self->stash(
     message => 'Visualização do repositório...',
     url_sha1 => $url_sha1,
     url => $repository->{url},
+    total_files => $total_files,
+    metrics => {
+      change_cost => $metric->{global_metrics}->{change_cost},
+      lcom4_mean => $metric->{global_metrics}->{lcom4_mean},
+      total_loc => $metric->{global_metrics}->{total_loc},
+    },
   );
 }
 
