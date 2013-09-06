15b473687b44a3f56902430a02be1de81c8b47f8
Joenio Costa <joenio@colivre.coop.br>
2013-09-05 22:25:00 -0300

    add zoom support; bug fixes

--- lib/Visualizo/Repository.pm
+++ lib/Visualizo/Repository.pm
@@ -1,21 +1,12 @@
 package Visualizo::Repository;
 use Mojo::Base 'Mojolicious::Controller';
 use Digest::SHA qw(sha1_hex);
+use CodeJuicer::DB;
 
 sub enqueue {
   my $self = shift;
-
   my $url = $self->param('url');
   my $url_sha1 = sha1_hex($url);
-
-  # 1) conecte no Avizo, passe a URL para ele, solicite FETCH
-  # pegue o seu retorno em relação ao status da tarefa (fetch,
-  # metrics, graphs, ...).
-  # 2) O Avizo deve automaticamente executar as tarefas de analisar,
-  # gerar metricas e graficos após o download.
-  # 3) O Visualizo em sí deve evitar ao máximo armazenar informações,
-  # se possível ele nem terá um banco de dados.
-
   $self->stash(
     message => 'Aguarde, estamos fazendo download do repositório...',
     url_sha1 => $url_sha1,
@@ -24,20 +15,13 @@ sub enqueue {
 
 sub visualization {
   my $self = shift;
-
   my $url_sha1 = $self->param('url_sha1');
-
-  # 1) conecte no Avizo, passe a URL para ele, solicite FETCH
-  # pegue o seu retorno em relação ao status da tarefa (fetch,
-  # metrics, graphs, ...).
-  # 2) O Avizo deve automaticamente executar as tarefas de analisar,
-  # gerar metricas e graficos após o download.
-  # 3) O Visualizo em sí deve evitar ao máximo armazenar informações,
-  # se possível ele nem terá um banco de dados.
-
+  my $repositories = CodeJuicer::DB->c('repositories')->find({ _id => $url_sha1 });
+  my $repository = $repositories->next;
   $self->stash(
     message => 'Visualização do repositório...',
     url_sha1 => $url_sha1,
+    url => $repository->{url},
   );
 }
 
